3fbb0da8165da4a06565e5c5d782babfc16cdf8c
Mateus Pires <mateus.dev.ti@gmail.com>
2024-12-12 12:19:48 -0300

    toast para testar no mob

--- app/_components/GetData/index.tsx
+++ app/_components/GetData/index.tsx
@@ -41,6 +41,12 @@ function GetData({
         });
       } catch (error) {
         console.error("Error sending page view data via API route:", error);
+        toast({
+          title: "Scheduled: Catch up",
+          description: "Friday, February 10, 2023 at 5:57 PM",
+
+          variant: "destructive",
+        });
       }
     };
 
